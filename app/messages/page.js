0cfba62bb419eb05357ec1271b1191d5520a1742
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-25 20:01:11 +0530

    Caching complete -Request Memoization, Data Cache,

--- app/messages/page.js
+++ app/messages/page.js
@@ -5,14 +5,14 @@ import { getMessages } from "@/lib/messages";
 //export const revalidate = 5; // controlling data cache
 //export const dynamic = "force-dynamic"; // avoid data caching
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   //unstable_noStore(); //disable caching
   // const response = await fetch("http://localhost:8080/messages", {
   //   next: { tags: ["msg"] },
   // });
   // const messages = await response.json();
 
-  const messages = getMessages();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
